e97adecf387519d4ed51a873fa8254486da787a2
MohananT <mohan.stanky@gmail.com>
2019-06-02 13:21:44 +0530

    save data to fire store

--- App/app.js
+++ App/app.js
@@ -1,5 +1,38 @@
-var cafes;
+var cafeList = document.querySelector('#cafe-list');
+var form = document.querySelector('#add-cafe-form');
+
+function renderCafe(doc) {
+    let data = doc.data();
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let city = document.createElement('span');
+    let cross = document.createElement('div');
+
+    name.textContent = data.name;
+    city.textContent = data.city;
+    cross.textContent = 'x';
+
+    li.appendChild(name);
+    li.appendChild(city);
+    li.appendChild(cross);
+    cafeList.appendChild(li);
+}
+
+// get data from firestore
 db.collection('cafes').get()
 .then((snapshot) => {
-    console.log(snapshot.docs);
+    snapshot.docs.forEach(doc => {
+        renderCafe(doc);
+    });
+});
+
+// save data to firestore
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('cafes').add({
+        name: form.name.value,
+        city: form.city.value
+    });
+    form.name.value = '';
+    form.city.value = '';
 });
